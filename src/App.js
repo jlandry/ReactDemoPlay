2a70bd83b90dd50d1519cb2601920dcd5415e3ea
jonathanlandry <landry_jonathan@bah.com>
2018-08-15 13:45:10 -0400

    Added React Demo

--- src/App.js
+++ src/App.js
@@ -1,35 +1,54 @@
 import React from 'react';
 import './App.css';
 import StateTesting from './StateTesting/StateTesting'
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { ConnectedRouter } from 'react-router-redux';
+import { Provider } from 'react-redux'
 import TicTacToe from './TicTacToe/TicTacToe';
+import Store from './ReactDemo/Store';
+// const store = createStore(Counter)
 
 class App extends React.Component {
   render () {
    return (
+     <div>
+    {/* // <Provider store={store}> */}
+    {/* <ConnectedRouter >  */}
     <Router>
       <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/statetesting">State Testing</Link>
-        </li>
-        <li>
-          <Link to="/tictactoe">Tic Tac Toe</Link>
-        </li>
-      </ul>
-
-      <hr />
-
-      <Route exact path="/" component={StateTesting} />
-      <Route path="/statetesting" component={StateTesting} />
-      <Route path="/tictactoe" component={TicTacToe} />
-    </div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/statetesting">State Testing</Link>
+          </li>
+          <li>
+            <Link to="/tictactoe">Tic Tac Toe</Link>
+          </li>
+        </ul>
+        <hr />
+          {/* <Switch> */}
+        <Route exact path="/" component={StateTesting} />
+        <Route path="/statetesting" component={StateTesting} />
+        <Route path="/tictactoe" component={TicTacToe} />
+      </div>
     </Router>
+      {/* </Switch> */}
+    {/* </ConnectedRouter> */}
+  {/* // </Provider> */}
+    </div>
    )
   }
 }
 
+    // <Router>
+    //   <div>
+     
+
+    //   <Route exact path="/" component={StateTesting} />
+    //   <Route path="/statetesting" component={StateTesting} />
+    //   <Route path="/tictactoe" component={TicTacToe} />
+
+    // </Router>
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import 'typeface-roboto'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+// import Redux from 'redux';
+// import Reducer from './ReactDemo/Reducer'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
